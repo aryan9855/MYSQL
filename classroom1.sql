1b313f6ec6123d3c93e2d10b1c2e5c7b52dcadc2
aryan9855 <aryansinghalagra@gmail.com>
2025-02-12 00:46:31 +0530

    12|02|2025

---
+++ classroom1.sql
@@ -0,0 +1,17 @@
+create database college;
+use college;
+
+create table student (
+rollno int primary key,
+name varchar(50)
+);
+
+select * from student;
+
+insert into student values
+(101,"Aryan"),
+(102,"Mahak"),
+(103,"Rohan");
+
+insert into student values (104,"Vaibhav");
+
